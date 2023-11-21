6ee8a0ec4d6819ff25f92d7474c7b0059eacabef
Lukefulton02 <143756157+Lukefulton02@users.noreply.github.com>
2023-11-21 04:06:39 -0500

    Update Source.cpp
    Added Three classes which will count as inheritance. These can be seen at the top but also have adjacent code at the bottom in a newly added case case 4.
    
    I also added another mini game thats just a question from hw 3 of 410. If you solve it you can pretty much win otherwise you go into crippling debt. This counts as higher level math and a static function.
    
    Im pretty sure there was something else I added but I can quite remember. I believe there was another criterion that got met through this process. If my spreadsheet is right we are at 29.5/30 points. If someone wants to go through it again feel free otherwise i will. Ask chat gpt about each criteria individually and make sure it shows you what its talking about. 
    
    Read the prompts I wrote and dull em if they're too much. 
    
    Only thing that really needs to change is if you put in a value greater than 5 or you have an invalid input for food it just continues on when it should repeat the entry process. Chat can prolly do this I just dont wanna right now. Ill do it later if need be.

--- Source.cpp
+++ Source.cpp
@@ -3,9 +3,61 @@
 #include <cstdlib>
 #include <string>
 #include <vector>
+#include <cmath>
+
 
 using namespace std;
 
+//Internal mini games.
+//class game, mathgame, and ecounter are a form of inheretenc
+class Game {
+public:
+    virtual void play() = 0; // Pure virtual function for playing the game
+    virtual ~Game() {} // Virtual destructor
+};
+
+class MathGame : public Game {
+public:
+    void play() override {
+        // Implement the math game logic here
+        int num1 = rand() % 10;
+        int num2 = rand() % 10;
+
+        cout << "Math Game: A homeless man offers you a deal. Answer these riddles three and theres a chance you might go free. If a man has " << num2 << " grams of crack on Tuesday and " << num1 << " grams of crack on Wednesday, how much doid he have on Monday? ";
+        int userAnswer;
+        cin >> userAnswer;
+
+        if (userAnswer == (num2 - num1) + num2) {
+            cout << "Correct! Heres some blow!" << endl;
+        } else {
+            cout << "Incorrect! Gimme your wallet." << endl;
+        }
+    }
+};
+
+class Encounter : public Game {
+public:
+    void play() override {
+        // Implement the encounter logic here
+        cout << "You encountered a wild animal! What will you do?" << endl;
+        cout << "1. Try to run away\n";
+        cout << "2. Stand your ground\n";
+        int choice;
+        cin >> choice;
+
+        if (choice == 1) {
+            cout << "You managed to escape the encounter." << endl;
+        } else {
+            cout << "The wild animal attacks you! You lose 20% health." << endl;
+            // Update player's health or take other relevant actions
+        }
+    }
+};
+
+
+
+
+// Main Game code   /////////////////////////////////////////////////////////////////////////////////////////////////////
 class Traveler {
 public:
     string name;
@@ -68,9 +120,66 @@ vector<string> events = {
     "You walked by a trainwreck, and your health and food suffered from toxic chemical exposure. Lose 20% health and 20 lbs of food.", // (31)
     "You misplaced your cartoon looking money bag with a big \"$\" on it. Lose $75.", // (32)
     "You encountered a pack of wolves while traveling. Lose 10% health and 15 lbs of food.", // (33)
-    "You got caught in a sharknado, which damaged your wagon and supplies. Lose $50 and 20 lbs of food." // (34)
-    "You lose your EZ Pass and have to pay a fine for skipping the toll. Lose $80." // (35)
+    "You got caught in a sharknado, which damaged your wagon and supplies. Lose $50 and 20 lbs of food.", // (34)
+    "You lose your EZ Pass and have to pay a fine for skipping the toll. Lose $80.", // (35)
+    "A Man... no... a HORRIFIC Man closes in on you in the night. As he approaches you notice his lanky features, his dark hair, his bulging moustache.... ITS DAVID HALL!! He lunges and latches on to your hair. Your stuck. Trapped. He whispers calmly in your ear ''Young man Tell me the thermal and propulsive efficieny of this Ramjet engine. If you succeed Ill give you a big wet kiss on the cheek. If you fail...'' He stops talking. David then gets right infront of you. You can feel his breath and more than that his stare, his contempt, his disgust. He hands you a paper and a pencil. You look at him confused and lost and he says to you ''If you dont have the equations memorized, then what are you doing here?'' Shaking, you look back down at the paper. On the paper it says:" // (36)
 };
+// Dr. Hall Mini Game of Doom. Chances of survival are slim to none. God speed./////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//This is a form of static function (also high level math)
+static void solveAerospaceProblem(Traveler& traveler) {
+    // Generate random values for aerospace variables
+    double gamma = 1.0 + static_cast<double>(rand()) / RAND_MAX * 0.7; // Random value between 1 and 1.7
+    double Ta = 200.0 + static_cast<double>(rand()) / RAND_MAX * 100.0; // Random value between 200 and 300
+    double To4 = 2000.0 + static_cast<double>(rand()) / RAND_MAX * 1000.0; // Random value between 2000 and 3000
+    double M = 0.8 + static_cast<double>(rand()) / RAND_MAX * 4.2; // Random value between 0.8 and 5
+
+    // Calculate the correct values for nth and np
+    double To2 = Ta * (1 + ((gamma - 1) / 2) * pow(M, 2));
+    double Te = To4 / (1 + ((gamma - 1) / 2) * pow(M, 2));
+    double correctNth = ((To4 - Te) - (To2 - Ta)) / (To4 - To2);
+    double correctNp = 2 / (1 + sqrt((To4 - Te) / (To2 - Ta)));
+
+    // Display the prompt to the user
+    cout << "Given values: gamma = " << gamma << ", Ta = " << Ta << " K, To4 = " << To4 << " K, M = " << M << endl;
+    cout << "Use these values to calculate thermal efficiency (nth) and propulsion efficiency (np)." << endl;
+
+    // Get user input
+    double userNth, userNp;
+    cout << "Enter your calculated value for nth: ";
+    cin >> userNth;
+    cout << "Enter your calculated value for np: ";
+    cin >> userNp;
+
+    // Check the correctness of the user's input
+    double tolerance = 0.01; // Tolerance of 1% for correctness
+    if (fabs(userNth - correctNth) < tolerance && fabs(userNp - correctNp) < tolerance) {
+        // Correct input, gain money
+        cout << "Congratulations! (Smooch) Your calculations are correct! You really are an Aerospace Engineer! Cherio! Dr Hall then leaves, but you will never be the same. Forever changed by his presence. The PTSD of this ecounter will be felt three generations down the line. I am so sorry." << endl;
+        traveler.money += 52000;
+        traveler.health += 10;
+    } else {
+        // Incorrect input, lose money and health
+        cout << "Incorrect! Dr. Hall unhinges his jaw like an Anaconda and swollows you whole." << endl;
+        traveler.health = 0;
+        traveler.money -= 52000;
+    }
+}
+// End of Dr. Hall Mini Game of Doom. Thank god.///////////////////////////////////////////////////////////??????
+
+
+void displayMap(const Traveler& player) {
+    cout << "\n===== Ohio Trail Map =====" << endl;
+    cout << "Cities: ";
+    for (const City& city : cities) {
+        cout << city.name << " (" << city.distance << " miles) ";
+    }
+    cout << endl;
+    cout << "Your Location: " << cities[player.distance / 100].name << endl;
+    cout << "Distance to Destination: " << 200 - player.distance << " miles" << endl;
+    cout << "======================================" << endl;
+}
+
+
 
 void simulateEvent(Traveler& traveler, int& lastEventIndex) {
     int eventIndex;
@@ -209,19 +318,16 @@ void simulateEvent(Traveler& traveler, int& lastEventIndex) {
     else if (eventIndex == 35) {
         traveler.money -= 80;
     }
+    else if (eventIndex == 36) {
+            solveAerospaceProblem(traveler);
+    }
 }
 
-void displayMap(const Traveler& player) {
-    cout << "\n===== Ohio Trail Map =====" << endl;
-    cout << "Cities: ";
-    for (const City& city : cities) {
-        cout << city.name << " (" << city.distance << " miles) ";
-    }
-    cout << endl;
-    cout << "Your Location: " << cities[player.distance / 100].name << endl;
-    cout << "Distance to Destination: " << 200 - player.distance << " miles" << endl;
-    cout << "======================================" << endl;
-}
+
+
+
+
+
 
 int main() {
     srand(time(0));
@@ -260,7 +366,7 @@ int main() {
         
 
         if (player.food < 0 || player.health <= 0 || player.money < 0) {
-            cout << "Game Over! " << player.name << " couldn't make it to Cleveland, Ohio. You suck :)" << endl;
+            cout << "Game Over! " << player.name << " couldn't cut it in Ohio. You suck :)" << endl;
             break;
         }
 
@@ -269,6 +375,10 @@ int main() {
         cout << "1. Buy Food\n";
         cout << "2. Rest\n";
         cout << "3. Continue\n";
+        // Check if the encounter option should be available
+                if (lastEventIndex == 2) { // Change this condition to match the specific event index
+                    cout << "4. Encounter\n"; // New option
+                }
         cout << "Enter your choice: ";
         cin >> choice;
 
@@ -302,13 +412,26 @@ int main() {
         case 3:
             break;
 
-        default:
-            cout << "Invalid choice. Please choose a valid option." << endl;
+         
+        case 4:
+                // Play the Encounter only when the specific event occurs
+            if (lastEventIndex == 2) { // Change this condition to match the specific event index
+                Encounter wildAnimalEncounter;
+                wildAnimalEncounter.play();
+            } else {
+                cout << "No encounter available at the moment." << endl;
+            }
+                break;
+                
+                default:
+                    cout << "Invalid choice. Please choose a valid option." << endl;
+                
         }
 
         cout << "\nPress Enter to continue...";
         cin.get();
         cin.ignore();
+        
     }
 
     if (player.distance >= 200) {
